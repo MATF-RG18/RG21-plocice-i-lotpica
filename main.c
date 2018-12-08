8fcc72ea26f53db4ee44cae71be074514c15caff
Mateja <matejarkvc@gmail.com>
2018-12-09 00:34:52 +0100

    Screenshot ned4

--- main.c
+++ main.c
@@ -1,44 +1,57 @@
 #include <GL/glut.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
+#include <time.h>
 #define pi 3.14
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_timer(int value);
+static void on_timer2(int value);
+static void on_timer3(int value);
+static void on_timer4(int value);
 static void on_display(void);
 static void on_reshape(int width, int height);
-
+static float x_curr, y_curr; 
+const static float r = 20;
+static float x_k =25;
+static float y_k = 25;
 static float x;
 static float y;
 static float i;
 static float b;
-static int promena = 0;
+
 static float phi, theta;
 static float delta_phi, delta_theta;
 static int timer_active;
 static int timer_active2;
+static int timer_active3;
 static void on_display(void);
 int main(int argc,char** argv)
 {
 
-    //Inicijalizacija prozora
-    glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);   
-    glutInitWindowSize(1300,800);
+	//Inicijalizacija prozora
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);	
+	glutInitWindowSize(1300,800);
 
-    glutCreateWindow(argv[0]);
-    
-      /* Incijalizuju se globalne promenljive. */
+	glutCreateWindow(argv[0]);
+	
+	  /* Incijalizuju se globalne promenljive. */
     phi = theta = 0;
     delta_phi = delta_theta = pi / 90;
     glClearColor(0.75, 0.75, 0.75, 0);
-    glutKeyboardFunc(on_keyboard);
+	glutKeyboardFunc(on_keyboard);
     glutReshapeFunc(on_reshape);
-    glutDisplayFunc(on_display);
+	glutDisplayFunc(on_display);
+    x_curr = 0;
+    y_curr = 0;
 
-
+    x_k =5;
+    y_k =5;
+    glutTimerFunc(50, on_timer4, 0);
     glEnable(GL_DEPTH_TEST);
-    glutMainLoop();
-    return 0;
+	glutMainLoop();
+	return 0;
 }
 static void on_reshape(int width, int height)
 {
@@ -52,63 +65,46 @@ static void on_reshape(int width, int height)
 }
 static void on_display(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    //glColor3f(0,0,1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0, 0, 1000, 0, 0, 0, 0, 1, 0);
-   glColor3f(0.9, 0.5, 0.5);
-
+    /*gluLookAt(1100 * cos(theta) * cos(phi),
+              1100 * cos(theta) * sin(phi),
+             1100 + 220*5 * sin(theta),
+             0,0,0,
+              0, 1, 0);*/
+    gluLookAt(0, 0, 1100, 0, 0, 0, 0, 1, 0); 
     glPushMatrix();
-       glTranslatef(1.5, 4, 0);
-       glutSolidSphere(0.2, 10, 10);
-       glTranslatef(0.1, 0, 0);
-       glutSolidSphere(0.16, 10, 10);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslatef(-800,x,0);
-        glRotatef(90, 1, 0, 0);
+        glTranslatef(-880,x,0);
         glColor3f(0, 0, 1);
-         gluLookAt(5 * cos(theta) * cos(phi),
-              5* cos(theta) * sin(phi),
-              5* sin(theta),
-              0, 0, 0, 0, 1, 0);
-        glRotatef(90, 0, 0, 1);
+        glScalef(0.2,2,0.2);
         glutWireCube(100);
     glPopMatrix();
     glPushMatrix();
-        glTranslatef(800,y,0);
+        glTranslatef(880,y,0);
         glColor3f(0, 1, 0);
-         gluLookAt(5 * cos(theta) * cos(phi),
-              5* cos(theta) * sin(phi),
-              5* sin(theta),
-              0, 0, 0, 0, 1, 0);
+        glScalef(0.2,2,0.2);
         glutWireCube(100);
     glPopMatrix();
     glPushMatrix();
         glColor3f(1,0,0);
-         gluLookAt(5 * cos(theta) * cos(phi),
-              5* cos(theta) * sin(phi),
-              5* sin(theta),
-              0, 0, 0, 0, 1, 0);
-        glutSolidSphere(20,1000,1000);
+        glTranslatef(x_curr,y_curr,0);
+        glutSolidSphere(r,50,50);
     glPopMatrix();
-    /*
-    //Inicijalizacija plocica
-    glBegin(GL_POLYGON);
-        glVertex3f(0.97,0.3,0);
-        glVertex3f(0.92,0.3,0);
-        glVertex3f(0.92,-0.3,0);
-        glVertex3f(0.97,-0.3,0);
+    /*();
+        glColor3f(0.5,0.5,0.5);
+        glTranslatef(-20,0,0);
+        glScalef(2.3,1.5,0);
+        glutSolidCube(800);
+    glPopMatrix();*/
+     glColor3f(0, 0, 0);
+     glBegin(GL_LINE_LOOP);
+        glVertex3f(900, 580, 0);
+        glVertex3f(-900, 580, 0);
+        glVertex3f(-900, -580, 0);
+        glVertex3f(900,-580, 0);    
     glEnd();
-    glBegin(GL_POLYGON);
-        glVertex3f(-0.97,0.3,0);
-        glVertex3f(-0.92,0.3,0);
-        glVertex3f(-0.92,-0.3,0);
-        glVertex3f(-0.97,-0.3,0);
-    glEnd();*/
-    glutSwapBuffers();
+	glutSwapBuffers();
 }
 static void on_timer(int value)
 {
@@ -116,35 +112,53 @@ static void on_timer(int value)
         return;
 
     /* Provere i pomeranja Igraceve plocice*/
-    if((x >= -400 && i < 0) || (x<= 400 && i >0))
+    if((x+i >= -480 && i < 0) || (x+i<= 480 && i >0))
          x+=i;
-
-    glutPostRedisplay();
     if(timer_active)
-        glutTimerFunc(50, on_timer, 0);
+        glutTimerFunc(10, on_timer, 0);
 }
 static void on_timer2(int value)
 {
     if (value != 0)
         return;
-
-    if(promena ==0)
-    {
-        y+=b;
-        if(y>=400)
-            promena = 1;
-    }
-    else if(promena ==1)
-    {
-        y-=b;
-        if(y <= -400)
-            promena = 0;
-    }
-    glutPostRedisplay();
+    y+=b;
+    if(y+b >= 480 || y+b<= -480)
+        b*=-1;
     if(timer_active2)
-        glutTimerFunc(50, on_timer2, 0);
-
+        glutTimerFunc(10, on_timer2, 0);
 }
+static void on_timer3(int value)
+{
+    if (value != 0)
+        return;
+
+    y_curr += y_k;
+    if(y_curr>=560 || y_curr <=-560)
+        y_k*=-1;
+
+    x_curr-= x_k;
+   if((y_curr>=(x-100) && y_curr<=(x+100) && x_curr == -860) || (y_curr>=(x-100) && y_curr<=(x+100) && x_curr == 860))
+    {
+        x_k*=-1;
+    }
+   else if(x_curr <= -880 || x_curr >= 880)
+   {
+        x_curr = 0;
+        y_curr = 0;
+        timer_active3 = 0;
+        return;
+   }
+    if(timer_active3)
+        glutTimerFunc(20, on_timer3, 0);
+}
+static void on_timer4(int value)
+{
+    if (value != 0)
+        return;
+    glutPostRedisplay();
+    glutTimerFunc(50, on_timer4, 0);
+}
+
 static void on_keyboard(unsigned char key, int x, int y)
 {
     switch (key) {
@@ -155,18 +169,26 @@ static void on_keyboard(unsigned char key, int x, int y)
 
     case 'g':
             i = -25;
-            glutTimerFunc(50, on_timer, 0);
+            glutTimerFunc(10, on_timer, 0);
         break;
     case 'h':
             i =25;
-            glutTimerFunc(50, on_timer, 0);
+            glutTimerFunc(10, on_timer, 0);
         break;
     case 'r': 
       if (!timer_active2) {
             timer_active2 = 1;
-            b = 25;
-            glutTimerFunc(50, on_timer2, 0);
+            b = 5;
+            glutTimerFunc(10, on_timer2, 0);
         }
+        break;
+    case 'G':
+        /* Pokrece se animacija. */
+        if (!timer_active3) {
+            glutTimerFunc(20, on_timer3, 0);
+            timer_active3 = 1;
+        }
+        break;
       case 'p':
         /* Dekrementira se ugao phi i ponovo iscrtava scena. */
         phi -= delta_phi;
@@ -190,8 +212,11 @@ static void on_keyboard(unsigned char key, int x, int y)
         break;
       case 'R':
         /* Resetuju se uglovi phi i theta na pocetne vrednosti. */
-        b=0;
-        i=0;
+        x=0;
+        y=0;
+        timer_active3=0;
+        timer_active2 = 0;
+        timer_active= 0;
         phi = theta = 0;
         glutPostRedisplay();
         break;
