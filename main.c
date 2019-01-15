28dda40b56a5695492bb524e7be550984056d8e7
Mateja <matejarkvc@gmail.com>
2019-01-15 19:57:38 +0100

    dodavanje texture

--- main.c
+++ main.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 #include <time.h>
+#include "image.h"
+#define FILENAME1 "fudteren.bmp"
+static GLuint names[2];
+static float matrix[16];
 #define pi 3.14
+static void displayText( float x, float y, int r, int g, int b, const char *string );
 static void on_keyboard(unsigned char key, int x, int y);
+static void initialize(void);
 static void on_timer(int value);
 static void on_timer2(int value);
 static void on_timer3(int value);
@@ -35,6 +42,7 @@ static float phi, theta;//uglovi kamere
 static int timer_active;// flag timer za igracevu plocicu
 static int timer_active2;// flag timer za racunar plocice
 static int timer_active3;// flag timer za lopticu
+static int timer_active4;// flag timer za globalni timer
 
 int main(int argc,char** argv)
 {
@@ -50,6 +58,7 @@ int main(int argc,char** argv)
     glClearColor(0.75, 0.75, 0.75, 0);
     glutKeyboardFunc(on_keyboard);
     glutReshapeFunc(on_reshape);
+    initialize();
     glutDisplayFunc(on_display);
     //Inicijalizacija pocetnik koordinata loptice
     x_curr = 0;
@@ -59,6 +68,7 @@ int main(int argc,char** argv)
     y_k =5;
     glutTimerFunc(50, on_timer4, 0);
     glEnable(GL_DEPTH_TEST);
+    
     glutMainLoop();
     return 0;
 }
@@ -85,8 +95,6 @@ static void on_display(void)
     glRotatef(theta,1,0,0);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
-
-
     //Korisnikova Plocica
     GLfloat ambient_coeffs1[] = { 0, 0, 0.2, 1 };
     GLfloat diffuse_coeffs1[] = { 0, 0, 0.6, 1 };
@@ -137,6 +145,48 @@ static void on_display(void)
     glEnd();
     draw_score_igrac(x_scoreI,y_scoreI);
     draw_score_racunar(x_scoreR,y_scoreR);
+    //Cliping ravan koja pomaze prilikom postavljanja texture
+    GLdouble v[4] = {1,0,0,0};
+    glPushMatrix();
+        glTranslatef(-900,0,0);
+        glClipPlane(GL_CLIP_PLANE1,v);
+        glEnable(GL_CLIP_PLANE1);
+    glPopMatrix();
+    
+    glBindTexture(GL_TEXTURE_2D, names[1]);
+    glBegin(GL_QUADS);
+        glNormal3f(0, 0, 1);
+
+        glTexCoord2f(0, 0);
+        glVertex3f(-990, -580, 0);
+
+        glTexCoord2f(0,1);
+        glVertex3f(-990, 580, 0);
+
+        glTexCoord2f(1, 1);
+        glVertex3f(900, 580, 0);
+
+        glTexCoord2f(1, 0);
+        glVertex3f(900, -580, 0);
+    glEnd();
+    glDisable(GL_CLIP_PLANE1);
+    /* Iskljucujemo aktivnu teksturu */
+    glBindTexture(GL_TEXTURE_2D, 0);
+    displayText(-1300,300,1,0,0,"e - easy mode");
+    displayText(-1300,250,1,0,0,"r - default mode");
+    displayText(-1300,200,1,0,0,"h - hard mode");
+       if(score_racunar == 5)
+    {
+        displayText(-300,700,1,0,0,"Pobednik je Racunar :(!!");
+        displayText(-300,650,1,0,0,"Pritisni taster \'R\' za novu igru");
+        timer_active4=1;
+    }
+     if(score_igrac == 5)
+    {
+        displayText(-300,700,1,0,0,"BRAVO BRAVO TI SI POBEDNIK!");
+        displayText(-300,650,1,0,0,"Pritisni taster \'R\' za novu igru");
+        timer_active4=1;
+    }
     glutSwapBuffers();
 }
 static void on_timer(int value)
@@ -172,7 +222,7 @@ static void on_timer2(int value)
     if(y+b >= 480 || y+b<= -480)
         b*=-1;
     if(timer_active2)
-        glutTimerFunc(10, on_timer2, 0);
+        glutTimerFunc(20, on_timer2, 0);
 }
 static void on_timer3(int value)
 {
@@ -218,7 +268,8 @@ static void on_timer4(int value)
     // ovaj timer sluzi kako bi u svakom momentu timera iscrtavao glutPostRedisplay 
     // da ne bi dolazilo do "seckanja"
     glutPostRedisplay();
-    glutTimerFunc(50, on_timer4, 0);
+    if(!timer_active4)
+        glutTimerFunc(50, on_timer4, 0);
 }
 
 static void on_keyboard(unsigned char key, int x, int y)
@@ -239,26 +290,26 @@ static void on_keyboard(unsigned char key, int x, int y)
         break;
     case 'r': 
     //pokrecemo racunar plocicu u default modu 
+     b = 3;
       if (!timer_active2) {
             timer_active2 = 1;
-            b = 3;
-            glutTimerFunc(10, on_timer2, 0);
+            glutTimerFunc(20, on_timer2, 0);
         }
         break;
       case 'e': 
     //pokrecemo racunar plocicu u easy modu
+      b = 1;
       if (!timer_active2) {
             timer_active2 = 1;
-            b = 2;
-            glutTimerFunc(10, on_timer2, 0);
+            glutTimerFunc(20, on_timer2, 0);
         }
         break;   
     case 'h': 
     //pokrecemo racunar plocicu u hard modu
+     b = 5;
       if (!timer_active2) {
             timer_active2 = 1;
-            b = 5;
-            glutTimerFunc(10, on_timer2, 0);
+            glutTimerFunc(20, on_timer2, 0);
         }
         break; 
     case 'G':
@@ -274,12 +325,18 @@ static void on_keyboard(unsigned char key, int x, int y)
         score_igrac = 0;
         x=0;
         y=0;
+        x_curr = 0;
+        y_curr = 0;
         phi =0;
         theta =0;
+        timer_active4=0;
         timer_active3=0;
         timer_active2 = 0;
         timer_active= 0;
         glutPostRedisplay();
+        //if(!timer_active4)
+        //glutTimerFunc(50, on_timer4, 0);
+
         break;
     //tasteri p,P,t,T sluze za rotiranje kamere(odnosno scene)
       case 'p':
@@ -339,7 +396,6 @@ static void draw_score_racunar(float x,float y)
             glutSolidSphere(25,50,50);
         glPopMatrix();
     }
-    glDisable(GL_CLIP_PLANE1);
 }
 static void draw_score_igrac(float x,float y)
 {   
@@ -378,5 +434,67 @@ static void draw_score_igrac(float x,float y)
             glutSolidSphere(25,50,50);
         glPopMatrix();
     }
-    glDisable(GL_CLIP_PLANE1);
+}
+
+static void initialize(void)
+{
+    /* Objekat koji predstavlja teskturu ucitanu iz fajla. */
+    Image * image;
+
+    /* Postavlja se boja pozadine. */
+
+    /* Ukljucuje se testiranje z-koordinate piksela. */
+    glEnable(GL_DEPTH_TEST);
+
+    /* Ukljucuju se teksture. */
+    glEnable(GL_TEXTURE_2D);
+
+    glTexEnvf(GL_TEXTURE_ENV,
+              GL_TEXTURE_ENV_MODE,
+              GL_REPLACE);
+
+    /*
+     * Inicijalizuje se objekat koji ce sadrzati teksture ucitane iz
+     * fajla.
+     */
+    image = image_init(900, 580);
+
+
+    /* Generisu se identifikatori tekstura. */
+    glGenTextures(2, names);
+
+
+    /* Kreira se druga tekstura. */
+    image_read(image, FILENAME1);
+
+    glBindTexture(GL_TEXTURE_2D, names[1]);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D,
+                    GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                 image->width, image->height, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image->pixels);
+
+    /* Iskljucujemo aktivnu teksturu */
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    /* Unistava se objekat za citanje tekstura iz fajla. */
+    image_done(image);
+
+    /* Inicijalizujemo matricu rotacije. */
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glGetFloatv(GL_MODELVIEW_MATRIX, matrix);
+}
+static void displayText( float x, float y, int r, int g, int b, const char *string ) {
+    int j = strlen( string );
+ 
+    glColor3f( r, g, b );
+    glRasterPos3f( x, y ,0);
+    for( int i = 0; i < j; i++ ) {
+        glutBitmapCharacter( GLUT_BITMAP_TIMES_ROMAN_24, string[i] );
+    }
 }
